957d9f1cdb5e83834297595500dd26f9cd749186
Zaky Al Askary <mz.askary@proton.me>
2026-04-23 09:16:08 +0700

    setup environment

---
+++ 26.04.23 - Test-Driven Development/p2p-lending/src/test/java/com/p2p/AppTest.java
@@ -0,0 +1,13 @@
+package com.p2p;
+
+// Import untuk JUnit 5
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
